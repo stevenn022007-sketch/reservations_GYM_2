6825efa808fba5091760b817aaeca87c02614947
Stefy705 <stefhguz702@gmail.com>
2026-06-02 11:27:08 -0500

    Add: funciones de miembros

---
+++ modules/miembros.py
@@ -0,0 +1,99 @@
+# --------------CRUD--------MIEMBROS-----------------
+from rich.console import Console
+# Qué necesita un miembro? -> id, nombre, tipo de suscripción
+# console = Console()  # Inicializamos la consola de Rich para usarla en este módulo
+#rich es una biblioteca de Python que permite crear interfaces de usuario en la terminal con estilos y colores. 
+# En este código, se utiliza para imprimir mensajes estilizados y para manejar la entrada del usuario de manera más atractiva.
+console = Console()
+# -------VER MIEMBROS-----------------
+
+ #-----------STEFYY- CREAR MIEMBRO----------------
+
+#escribimos el mismo diccionario que esta en main para trabajar sobre los mismos datos
+def crear_miembro(gimnasio):
+    console.print("\n[bold cyan]➕ CREAR NUEVO MIEMBRO[/bold cyan]")
+
+    nombre = console.input("[green]Nombre del miembro: [/green]").strip()
+
+    console.print("[green]Tipo de suscripción:[/green]")
+    console.print("  [bold]1.[/bold] Mensual")
+    console.print("  [bold]2.[/bold] Anual")
+
+    try:
+        opcion = int(console.input("[green]Elige (1 o 2): [/green]"))
+
+        if opcion == 1:
+            tipo = "Mensual"
+        elif opcion == 2:
+            tipo = "Anual"
+        else:
+            console.print("[red]⚠ Opción inválida.[/red]")
+            return
+
+        # Generar el ID automático contando cuántos miembros hay.
+
+        nuevo_id = f"miembro_{len(gimnasio) + 1}"
+
+        # Guardar el nuevo miembro en el diccionario
+        gimnasio[nuevo_id] = {
+            "nombre": nombre,
+            "tipo_suscripcion": tipo
+        }
+
+        console.print(f"[bold ]✅ Miembro '{nombre}' creado con ID {nuevo_id}[/bold]\n")
+
+    except ValueError:
+        console.print("[red]⚠ Error: Ingresa solo números donde se piden.[/red]")
+
+#----------------------ELIMINAR MIEMBRO----------------------
+
+def eliminar_miembro(lista_miembros):
+    eliminar_por_id = input("Ingrese el ID del miembro que deseas eliminar: ").lower()
+    
+    if eliminar_por_id in lista_miembros:
+        lista_miembros.pop(eliminar_por_id)
+        print(f"El ID {eliminar_por_id} fue eliminado exitosamente")
+        
+    else:
+        print(f"El ID {eliminar_por_id} no se encuentra en la base de datos")
+
+
+# -------------------EDITAR MIEMBRO----------------------
+
+def actualizar_miembro (gimnasio: dict):
+    id_miembro = int(console.input("[green] id del miembro a editar: [/green]"))
+    
+    # nombre_clase = input("Nombre de la clase: ").strip()
+
+    str_id_miembro = f"miembro_{id_miembro}"
+
+    if str_id_miembro in gimnasio:
+        info = gimnasio[str_id_miembro]
+        print(f"¡Mienbro encontrado!. proporciona los datos a editar del miembro: {str_id_miembro}")
+        print("\nPresiona 'ENTER' sin escribir nada para mantener el valo actual\n")
+
+        console.print(f"[green]El Nombre actual es:[/green] {info["nombre"]}")
+        nuevo_nombre = console.input("[green]Elije el nuevo nombre, de lo contrario Presiona ENTER:[/green]")
+
+        if nuevo_nombre != "":
+            info["nombre"] = nuevo_nombre
+
+        console.print(f"[green]Tipo de suscripción Actual:[/green] {info["tipo_suscripcion"]}")
+        console.print("  Las Opciones para actualizar son:")
+        console.print("  [bold]1.[/bold] Mensual")
+        console.print("  [bold]2.[/bold] Anual")
+        nuevo_plan = int(console.input("[green]Elige (1 o 2) para actualizar, de lo contrario presiona ENTER: [/green]"))
+
+        if nuevo_plan != "":
+            if nuevo_plan == 1:
+                tipo = "Mensual"
+            elif nuevo_plan == 2:
+                tipo = "Anual"
+            else:
+                console.print("[red]⚠ Opción inválida.[/red]")
+                return
+        info["tipo_suscripcion"] = tipo
+    else:
+        print(f"❌❌❌El mienbro con ese id {str_id_miembro} no exsiste❌❌❌")
+
+
